a8f18fddbe90d36646413005cbed0fb49517218c
Thevesh <thevesh.theva@gmail.com>
2025-03-10 00:12:44 +0800

    Fix: Page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Joyce Lee, CFA",
+  title: "CFA, on the Way!",
   description: "CFA Progress Tracker",
 };
 
